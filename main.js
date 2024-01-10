a34d37ed799345321bb70fcea90d7e979d84c992
stray-code <abc1007020@gmail.com>
2024-01-10 23:14:46 +0900

    新しいファイル名を修正

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ const targetFileNameList = fileNameList.filter(fileName => {
   return fileName !== '.DS_Store'
 })
 
-targetFileNameList.forEach(fileName => {
+targetFileNameList.forEach((fileName, index) => {
   // MEMO: 新しいファイル名
-  const newName = fileName.split('_')[1];
+  const number = `000${index + 1 + 20 * 0}`.slice(-3)
+
+  const replacedName = fileName.replace(/(\d{4})(\d{2})(\d{2})(\d+)/g, '$1_$2_$3')
+
+  const newName = `${number}_${replacedName}`
+
+  // const newName = fileName.split('１_')[1];
 
   const before = path.join(dir, fileName);
   const after = path.join(dir, newName);
