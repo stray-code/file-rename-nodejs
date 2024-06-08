af29c39aadefa5ca82d4776ba63b682a3b6641ff
stray-code <abc1007020@gmail.com>
2024-06-08 13:08:57 +0900

    rename処理修正

--- main.js
+++ main.js
@@ -10,16 +10,14 @@ const targetFileNameList = fileNameList.filter(fileName => {
 
 targetFileNameList.forEach((fileName, index) => {
   // MEMO: 新しいファイル名
-  const number = `000${index + 1 + 20 * 0}`.slice(-3)
+  // const number = `000${index + 1 + 20 * 0}`.slice(-3)
+  // const replacedName = fileName.replace(/(\d{4})(\d{2})(\d{2})(\d+)/g, '$1_$2_$3')
+  // const newName = `${number}_${replacedName}`
 
-  const replacedName = fileName.replace(/(\d{4})(\d{2})(\d{2})(\d+)/g, '$1_$2_$3')
-
-  const newName = `${number}_${replacedName}`
-
-  // const newName = fileName.split('１_')[1];
+  const s = fileName.split('_');
 
   const before = path.join(dir, fileName);
-  const after = path.join(dir, newName);
+  const after = path.join(dir, `${s[1]}_${s[2]}_${s[3]}`);
 
   fs.rename(before, after, err => {
     if (err) {
